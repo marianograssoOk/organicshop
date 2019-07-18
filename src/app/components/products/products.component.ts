4e85fcd9ff949288187b12d5aa6044cdaea9ebcc
marianograsso <marianograsso1991@gmail.com>
2019-07-18 11:44:21 -0300

    Displaying Categories

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-products',
@@ -8,9 +9,11 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductsComponent {
   products$;
+  categories$;
 
-  constructor(productService: ProductService) {
+  constructor(productService: ProductService, categoryService: CategoryService) {
     this.products$ = productService.getAll();
+    this.categories$ = categoryService.getAll();
   }
 
 }
